5f34b75c3c0a9274344f225522a2efae4eb85c96
Ilya Nixan <thenixan@gmail.com>
2019-08-17 19:36:13 +0300

    Day 9 done

--- src/aoc/y2015/task_9.rs
+++ src/aoc/y2015/task_9.rs
@@ -81,11 +81,10 @@ pub fn run() {
     for i in 0..counts.len() {
         let from = &counts[i];
         let new_destinations: Vec<String> = counts.iter().filter(|r| r != &from).map(|s| s.clone()).collect();
-        let found_length = find_route(&routes, &new_destinations, &from);
+        let found_length = find_route(&routes, &new_destinations, &from, true);
         if found_length.is_some() {
             let found_length = found_length.unwrap();
             if result == 0 || result > found_length {
-                println!("Start: {}", from);
                 result = found_length;
             }
         }
@@ -93,10 +92,9 @@ pub fn run() {
     println!("{:?}", result)
 }
 
-fn find_route(routes: &Vec<Route>, destinations: &Vec<String>, from: &String) -> Option<usize> {
+fn find_route(routes: &Vec<Route>, destinations: &Vec<String>, from: &String, shortest: bool) -> Option<usize> {
     let mut length = 0;
     if destinations.is_empty() {
-        println!("Finish: {}", from);
         return Some(0);
     }
     for i in 0..destinations.len() {
@@ -107,12 +105,17 @@ fn find_route(routes: &Vec<Route>, destinations: &Vec<String>, from: &String) ->
         if route.is_some() {
             let route_length = route.unwrap().distance;
             let new_destinations: Vec<String> = destinations.iter().filter(|r| r != &to).map(|s| s.clone()).collect();
-            let found_length = find_route(routes, &new_destinations, &to).map(|l| l + route_length);
+            let found_length = find_route(routes, &new_destinations, &to, shortest).map(|l| l + route_length);
             if found_length.is_some() {
                 let found_length = found_length.unwrap();
-                if length == 0 || length > found_length {
-                    println!("To: {}", to);
-                    length = found_length;
+                if shortest {
+                    if length == 0 || length > found_length {
+                        length = found_length;
+                    }
+                } else {
+                    if length == 0 || length < found_length {
+                        length = found_length;
+                    }
                 }
             }
         }
@@ -126,5 +129,43 @@ fn find_route(routes: &Vec<Route>, destinations: &Vec<String>, from: &String) ->
 
 pub fn run_e() {
     let input = File::open("src/aoc/y2015/task_9").unwrap();
-    let _input = BufReader::new(input);
+    let input = BufReader::new(input);
+    let routes: Vec<Route> = input.lines()
+        .into_iter()
+        .filter_map(|l| l.ok())
+        .map(|l| {
+            let route_and_distance: Vec<&str> = l.split(" = ").collect();
+            let route_part: String = route_and_distance[0].to_string();
+            let distance = route_and_distance[1].parse::<usize>().unwrap();
+            let route: Vec<&str> = route_part.split(" to ").collect();
+            Route::new(route[0].to_string(), route[1].to_string(), distance)
+        })
+        .collect();
+
+
+    let counts = routes
+        .iter()
+        .fold(vec![], |mut acc, r| {
+            if !acc.contains(&r.to) {
+                acc.push(r.to.clone());
+            }
+            if !acc.contains(&r.from) {
+                acc.push(r.from.clone());
+            }
+            acc
+        });
+
+    let mut result = 0;
+    for i in 0..counts.len() {
+        let from = &counts[i];
+        let new_destinations: Vec<String> = counts.iter().filter(|r| r != &from).map(|s| s.clone()).collect();
+        let found_length = find_route(&routes, &new_destinations, &from, false);
+        if found_length.is_some() {
+            let found_length = found_length.unwrap();
+            if result == 0 || result < found_length {
+                result = found_length;
+            }
+        }
+    }
+    println!("{:?}", result)
 }
