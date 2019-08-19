3b2f666d6140cb85ef34a998a248e8831def7ff8
Ilya Linnik <thenixan@gmail.com>
2019-08-19 10:59:43 +0300

    Day 13

--- src/aoc/y2015/task_13.rs
+++ src/aoc/y2015/task_13.rs
@@ -135,6 +135,58 @@ fn iterate(terms: &Vec<ComputedTerms>, mut result: Vec<ComputedTerms>, subjects:
 
 
 pub fn run_e() {
-    let input = File::open("src/aoc/y2015/task_12").unwrap();
+    let input = File::open("src/aoc/y2015/task_13").unwrap();
     let input = BufReader::new(input);
+    let mut input: Vec<ComputedTerms> = input.lines()
+        .into_iter()
+        .filter_map(|l| l.ok())
+        .map(|l| l[..l.len() - 1].to_string())
+        .map(|l| l.split(" ").map(|l| l.to_string()).collect())
+        .map(|l: Vec<String>| {
+            let m = if l[2] == "gain" { 1 } else { -1 };
+            Terms { subject: l[0].clone(), score: l[3].parse::<i32>().unwrap() * m, target: l[10].clone() }
+        })
+        .fold(vec![], |acc, t| {
+            let mut contains = false;
+            let mut acc: Vec<ComputedTerms> = acc
+                .into_iter()
+                .map(|mut c| {
+                    if c.subjects.contains(&t.subject) && c.subjects.contains(&t.target) {
+                        c.score += t.score;
+                        contains = true;
+                    }
+                    c
+                })
+                .collect();
+            if !contains {
+                acc.push(ComputedTerms { subjects: vec![t.subject, t.target], score: t.score });
+            }
+            acc
+        });
+    input.sort_by(|a, b| b.score.cmp(&a.score));
+
+    let mut subjects = input.iter().fold(vec![], |mut acc, i| {
+        if !acc.contains(&i.subjects[0]) {
+            acc.push(i.subjects[0].clone());
+        }
+        if !acc.contains(&i.subjects[1]) {
+            acc.push(i.subjects[1].clone());
+        }
+        acc
+    });
+
+    for s in &subjects {
+        input.push(ComputedTerms { subjects: vec![s.clone(), "Me".to_string()], score: 0 });
+    }
+
+    subjects.push("Me".to_string());
+
+    let first = input.first().unwrap().clone();
+    let result = vec![first.clone()];
+
+    let result = iterate(&input, result, subjects.into_iter().filter(|l| !first.subjects.contains(l)).collect());
+
+    let result: i32 = result.iter().map(|c| c.score).sum();
+
+    println!("{:?}", result);
 }
