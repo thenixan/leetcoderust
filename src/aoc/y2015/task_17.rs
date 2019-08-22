a03cc0d469062bfd5967a4326403269d6bdeae1d
Ilya Linnik <thenixan@gmail.com>
2019-08-22 12:37:02 +0300

    Day 17

--- src/aoc/y2015/task_17.rs
+++ src/aoc/y2015/task_17.rs
@@ -72,14 +72,12 @@ pub fn run() {
 
 fn iterate(target: usize, containers: &Vec<Container>, current: usize, current_sum: usize) -> usize {
     let mut result = 0;
-    if current_sum < target {
-        for i in current..containers.len() {
-            let sum = current_sum + &containers[i];
-            if sum == target {
-                result += 1;
-            } else if sum < target {
-                result += iterate(target, containers, i + 1, sum);
-            }
+    for i in current..containers.len() {
+        let sum = current_sum + &containers[i];
+        if sum == target {
+            result += 1;
+        } else if sum < target {
+            result += iterate(target, containers, i + 1, sum);
         }
     }
     return result;
@@ -88,4 +86,33 @@ fn iterate(target: usize, containers: &Vec<Container>, current: usize, current_s
 pub fn run_e() {
     let input = File::open("src/aoc/y2015/task_17").unwrap();
     let input = BufReader::new(input);
+
+    let input: Vec<Container> = input.lines()
+        .filter_map(|l| l.ok())
+        .map(|l| l.parse::<Container>())
+        .filter_map(|c| c.ok())
+        .collect();
+
+    let result = iterate_e(150, &input, 0, 0, 0, (None, 0));
+
+    println!("Result: {}", result.1);
+}
+
+fn iterate_e(target: usize, containers: &Vec<Container>, current_pos: usize, current_sum: usize, current_count: usize, current_min: (Option<usize>, usize)) -> (Option<usize>, usize) {
+    let mut result = current_min;
+    if current_sum < target {
+        for i in current_pos..containers.len() {
+            let sum = current_sum + &containers[i];
+            if sum == target {
+                if result.0.is_none() || result.0.unwrap() > current_count + 1 {
+                    result = (Some(current_count + 1), 1)
+                } else if result.0.unwrap() == current_count + 1 {
+                    result.1 += 1;
+                }
+            } else if sum < target {
+                result = iterate_e(target, containers, i + 1, sum, current_count + 1, result);
+            }
+        }
+    }
+    return result;
 }
