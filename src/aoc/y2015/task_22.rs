d1a7b391af45eefdba4a7a2e44aeebb9c87e2a97
Ilya Nixan <thenixan@gmail.com>
2019-08-28 09:00:35 +0300

    More wip of 22

--- src/aoc/y2015/task_22.rs
+++ src/aoc/y2015/task_22.rs
@@ -1,7 +1,5 @@
 use std::fs::File;
-use std::io::{BufReader, BufRead};
-use std::collections::HashMap;
-use std::hash::Hash;
+use std::io::{BufRead, BufReader};
 
 #[derive(Clone, Debug)]
 struct Character {
@@ -14,15 +12,6 @@ struct Character {
 impl Character {
     fn player(hit_points: i16, mana: i16) -> Self { Character { hit_points, mana, damage: 0, armor: 0 } }
     fn boss(hit_points: i16, damage: i16) -> Self { Character { hit_points, damage, mana: 0, armor: 0 } }
-
-    fn heal(&self, heal: i16) -> Self {
-        Character {
-            hit_points: self.hit_points + heal,
-            mana: self.mana,
-            damage: self.damage,
-            armor: self.armor,
-        }
-    }
 
     fn damage(&self, damage: i16) -> Self {
         let damage_done = i16::max(damage - self.armor, 1);
@@ -43,60 +32,47 @@ impl Character {
         }
     }
 
-    fn recharge(&self, mana: i16) -> Self {
-        Character {
-            hit_points: self.hit_points,
-            mana: self.mana + mana,
-            damage: self.damage,
-            armor: self.armor,
-        }
-    }
-
-    fn armor(&self, armor: i16) -> Self {
-        Character {
-            hit_points: self.hit_points,
-            mana: self.mana,
-            damage: self.damage,
-            armor: self.armor + armor,
-        }
-    }
-
-    fn as_caster(&self, this_step: usize, buffs: &HashMap<usize, &Buff>) -> Self {
+    fn as_caster(&self, this_step: &usize, buffs: &Vec<&Effect>) -> Self {
         let mut mana = self.mana;
         let mut hit_points = self.hit_points;
-        let mut damage = self.damage;
         let mut armor = self.armor;
-        buffs.iter().for_each(|(created, buff)| {
-            mana += buff.mana();
-            hit_points += buff.heal();
-            if this_step - *created == 1 {
-                armor += buff.armor()
-            } else if this_step - *created == buff.steps() {
-                armor -= buff.armor()
+        buffs.iter().for_each(|effect| {
+            mana += effect.buff.mana();
+            hit_points += effect.buff.heal();
+            if this_step - effect.casted_at == 1 {
+                armor += effect.buff.armor()
+            } else if this_step - effect.casted_at == effect.buff.steps() {
+                armor -= effect.buff.armor()
             }
         });
         return Character {
             mana,
             hit_points,
-            damage,
             armor,
+            damage: self.damage,
         };
     }
-    fn as_target(&self, this_step: usize, buffs: &HashMap<usize, &Buff>) -> Self {
-        let mut mana = self.mana;
+    fn as_target(&self, _: &usize, buffs: &Vec<&Effect>) -> Self {
         let mut hit_points = self.hit_points;
-        let mut damage = self.damage;
-        let mut armor = self.armor;
-        buffs.iter().for_each(|(created, buff)| {
-            hit_points -= buff.damage()
+        buffs.iter().for_each(|effect| {
+            hit_points -= effect.buff.damage()
         });
         return Character {
-            mana,
             hit_points,
-            damage,
-            armor,
+            mana: self.mana,
+            damage: self.damage,
+            armor: self.armor,
         };
     }
+}
+
+struct Effect {
+    casted_at: usize,
+    buff: Box<Buff>,
+}
+
+impl Effect {
+    fn is_finished(&self, turn: &usize) -> bool { self.buff.steps() < turn - self.casted_at }
 }
 
 #[derive(PartialEq)]
@@ -109,6 +85,10 @@ enum Buff {
 }
 
 impl Buff {
+    fn effect(&self, step: usize) -> Effect {
+        Effect { casted_at: step, buff: Box::new(self.clone()) }
+    }
+
     fn cost(&self) -> i16 {
         match self {
             Buff::MagicMissle => 53,
@@ -160,10 +140,6 @@ impl Buff {
     }
 }
 
-impl Buff {
-    fn is_finished(&self, created_turn: &usize, turn: &usize) -> bool { self.steps() < turn - created_turn }
-}
-
 pub fn run() {
     let input = File::open("src/aoc/y2015/task_22").unwrap();
     let input = BufReader::new(input);
@@ -182,14 +158,14 @@ pub fn run() {
 
     let buffs = vec![Buff::MagicMissle, Buff::Drain, Buff::Shield, Buff::Poison, Buff::Recharge];
 
-    let result = game(&player, &boss, &buffs, HashMap::with_capacity(5), 0).unwrap();
+    let result = game(&player, &boss, &buffs, vec![], 0).unwrap();
 
     println!("Result: {}", result)
 }
 
-fn game(player: &Character, boss: &Character, buffs: &Vec<Buff>, active_buffs: HashMap<usize, &Buff>, step: usize) -> Option<i16> {
-    let player = player.as_caster(step, &active_buffs);
-    let boss = boss.as_target(step, &active_buffs);
+fn game(player: &Character, boss: &Character, buffs: &Vec<Buff>, effects: Vec<&Effect>, step: usize) -> Option<i16> {
+    let player = player.as_caster(&step, &effects);
+    let boss = boss.as_target(&step, &effects);
 
 //    if step >= 30 {
 //        return None;
@@ -201,17 +177,17 @@ fn game(player: &Character, boss: &Character, buffs: &Vec<Buff>, active_buffs: H
         return Some(0);
     }
 
-    let active_buffs: HashMap<usize, &Buff> = active_buffs.into_iter().filter(|(created, buff)| !buff.is_finished(created, &step)).collect();
+    let active_effects: Vec<&Effect> = effects.into_iter().filter(|e| !e.is_finished(&step)).collect();
 
     if step % 2 == 0 {
         let mut result = None;
         for i in 0..buffs.len() {
             let new_buff = &buffs[i];
-            if player.mana >= new_buff.mana() && !active_buffs.values().any(|b| b == &new_buff) {
-                let mut new_active_buffs = active_buffs.clone();
-                new_active_buffs.insert(step, new_buff);
+            if player.mana >= new_buff.mana() && !active_effects.iter().any(|e| e.buff == new_buff) {
+                let mut new_active_effects = active_effects.clone();
+                new_active_effects.push(&new_buff.effect(step));
                 let player = player.spend_mana(new_buff.cost());
-                let r = game(&player, &boss, buffs, new_active_buffs, step + 1).map(|c| {
+                let r = game(&player, &boss, buffs, new_active_effects, step + 1).map(|c| {
                     c + new_buff.cost()
                 });
                 if result.is_none() || (r.is_some() && r.unwrap() < result.unwrap()) {
@@ -225,7 +201,7 @@ fn game(player: &Character, boss: &Character, buffs: &Vec<Buff>, active_buffs: H
         return result;
     } else {
         let player = player.damage(boss.damage);
-        return game(&player, &boss, buffs, active_buffs, step + 1);
+        return game(&player, &boss, buffs, active_effects, step + 1);
     }
 }
 
